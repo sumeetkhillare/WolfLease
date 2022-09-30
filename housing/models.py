8db3f730a175cdfe29d44af74a8b1e227db71621
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2022-09-30 13:50:48 -0400

    apartment model added

--- housing/models.py
+++ housing/models.py
@@ -11,3 +11,14 @@ class Owner(models.Model):
     contact_number = models.CharField(max_length=12)
     contact_email = models.EmailField(max_length=30)
     password = models.CharField(max_length=50)
+
+
+class Apartment(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    address = models.CharField(max_length=256)
+    facilities = models.CharField(max_length=512)
+    owner_id = models.ForeignKey(to=Owner, on_delete=models.DO_NOTHING)
