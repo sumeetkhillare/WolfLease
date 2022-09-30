7ac49a3e04bd1f1f59506fc42526c12e62db4c89
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2022-09-30 13:51:26 -0400

    Added UserViewset

--- housing/views.py
+++ housing/views.py
@@ -3,3 +3,10 @@ from rest_framework import status, viewsets
 from housing.serializers import UserSerializer
 from housing.models import User
 
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
