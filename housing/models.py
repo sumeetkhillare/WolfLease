7f6078bbfb4c79085b79550a3518f4cef53d4819
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2022-09-30 13:50:19 -0400

    Owner model added

--- housing/models.py
+++ housing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 import uuid
 
+# Create your models here.
+class Owner(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_number = models.CharField(max_length=12)
+    contact_email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=50)
