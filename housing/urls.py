57e211060886ad7ddeb13f1286bb9ddabcce57a1
ameyagv <ameyav1234@gmail.com>
2022-10-04 16:10:08 -0400

    added URL routing for Flat

--- housing/urls.py
+++ housing/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register(r'users', views.UserViewSet, basename="user_viewset")
+router.register(r'flats', views.FlatViewSet, basename="flat_viewset")
 
 urlpatterns = [
     path('', include(router.urls)),
