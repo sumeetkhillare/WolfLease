2315d98cbcdb1b59133382f16cee5d9275a2fca0
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2022-10-04 11:22:08 -0400

    flat model added

--- housing/models.py
+++ housing/models.py
@@ -32,3 +32,15 @@ class Lease(models.Model):
     )
     lease_start_date = models.DateField()
     lease_end_date = models.DateField()
+
+class Flat(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    availability = models.BooleanField(default=False)
+    associated_apt_id = models.ForeignKey(to=Apartment, on_delete=models.DO_NOTHING)
+    lease_id = models.ForeignKey(to=Lease, on_delete=models.DO_NOTHING)
+    rent_per_room = models.IntegerField()
+    floor_number = models.IntegerField()
