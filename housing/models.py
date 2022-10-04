2a387bb5e9f1f3f8fa2b0ea2ef2f85dafdc336a0
Yash Sonar <46718837+Yash-567@users.noreply.github.com>
2022-10-04 11:22:44 -0400

    User Model added

--- housing/models.py
+++ housing/models.py
@@ -44,3 +44,20 @@ class Flat(models.Model):
     lease_id = models.ForeignKey(to=Lease, on_delete=models.DO_NOTHING)
     rent_per_room = models.IntegerField()
     floor_number = models.IntegerField()
+
+class User(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    flat_id = models.ForeignKey(to=Flat, on_delete=models.DO_NOTHING)
+    contact_number = models.CharField(max_length=12)
+    contact_email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=50)
+    user_type = models.CharField(max_length=20, default="Guest")
+    dob = models.DateField()
+    gender = models.CharField(default="M", max_length=2)
+    pref_smoking = models.CharField(default="N", max_length=2)
+    pref_drinking = models.CharField(default="N", max_length=2)
+    pref_veg = models.CharField(default="N", max_length=2)
