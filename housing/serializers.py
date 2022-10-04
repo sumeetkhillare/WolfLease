3b3d5d404818eb66959860f9b0271d79c50a217b
ameyagv <ameyav1234@gmail.com>
2022-10-04 16:12:47 -0400

    added Flat Serializer

--- housing/serializers.py
+++ housing/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = '__all__'
+
+class FlatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Flat
+        fields = '__all__'
