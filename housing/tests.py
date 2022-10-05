c044e2ce27272016dfe732f4c209229a254f3d0f
ameyagv <ameyav1234@gmail.com>
2022-10-05 15:42:46 -0400

    test case for Flat model

--- housing/tests.py
+++ housing/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
 
+from django.test import SimpleTestCase
+from housing import models
 # Create your tests here.
+class FlatTest(SimpleTestCase):
+    def testStringRep(self):
+        flat = models.Flat(rent_per_room=1800)
+        self.assertEqual(None == flat, False)
