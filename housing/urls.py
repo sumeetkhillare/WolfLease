9751f4d4e2f955c15a85290f2ba0842fa6771027
subodhgujar <subodhsgujar@gmail.com>
2022-10-05 10:27:06 -0400

    Added interested url.

--- housing/urls.py
+++ housing/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename="user_viewset")
 router.register(r'flats', views.FlatViewSet, basename="flat_viewset")
 router.register(r'owners', views.OwnerViewSet, basename="owner_viewset")
+router.register(r'interests', views.InterestedViewSet, basename="interests_viewset")
 
 urlpatterns = [
     path('', include(router.urls)),
