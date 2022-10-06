fd0e4b6dec3b2c4cd7fba0c3debc7296f9178a73
subodhgujar <subodhsgujar@gmail.com>
2022-10-05 22:24:26 -0400

    Added GET and POST tests for Owers model.

--- housing/tests.py
+++ housing/tests.py
@@ -1,8 +1,26 @@
+from urllib import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from housing.models import Owner
 
-from django.test import SimpleTestCase
-from housing import models
-# Create your tests here.
-class FlatTest(SimpleTestCase):
-    def testStringRep(self):
-        flat = models.Flat(rent_per_room=1800)
-        self.assertEqual(None == flat, False)
+class OwnerTests(APITestCase):
+    def test_create_owner(self):
+        """
+        Ensure we can create a new Owner object.
+        """
+        url = '/owners/'
+        data = {'contact_number': '1234567890', 'contact_email': 'test@testing.com', 'password': 'test'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Owner.objects.count(), 1)
+        self.assertEqual(Owner.objects.get().contact_email, 'test@testing.com')
+
+    def test_show_owner(self):
+        """
+        Ensure we can fetch a new Owner object.
+        """
+        url = '/owners/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Owner.objects.count(), 0)
