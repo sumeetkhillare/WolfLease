0465cfdfc10529917798270402faa47d19a4714e
Kunal Patil <kunalpatil1810@gmail.com>
2022-10-09 14:49:23 -0400

    updated apps documentation

--- housing/apps.py
+++ housing/apps.py
@@ -1,7 +1,14 @@
 from django.apps import AppConfig
 
+"""
+    This is the application starting point where you can register different modules. 
+"""
 
 class HousingConfig(AppConfig):
+    """
+    This is configuration for Housing Module.
+    
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'housing'
 
