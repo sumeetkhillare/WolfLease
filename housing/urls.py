8b8802833f2e5fcd0837c4b38eefcebc73d84d21
Kunal Patil <kunalpatil1810@gmail.com>
2022-10-09 15:11:13 -0400

    Removed unwanted comments

--- housing/urls.py
+++ housing/urls.py
@@ -4,13 +4,6 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-# router.register(r'users', views.UserViewSet, basename="user_viewset")
-# router.register(r'flats', views.FlatViewSet, basename="flat_viewset")
-# router.register(r'owners', views.OwnerViewSet, basename="owner_viewset")
-# router.register(r'interests', views.InterestedViewSet, basename="interests_viewset")
-# router.register(r'apartments', views.ApartmentViewSet, basename="apartment_viewset")
-# router.register(r'lease', views.LeaseViewSet, basename='lease_viewset')
-
 
 urlpatterns = [
     path('', include(router.urls)),
