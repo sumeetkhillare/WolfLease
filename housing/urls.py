16b1b8272492e474b8cdd7a2cefb5e4ea6f0787a
Rohan Shivesh <rvss28@gmail.com>
2022-10-08 23:17:27 -0400

    fix url bug

--- housing/urls.py
+++ housing/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'users', views.UserViewSet, basename="user_viewset")
+# router.register(r'users', views.UserViewSet, basename="user_viewset")
 # router.register(r'flats', views.FlatViewSet, basename="flat_viewset")
 # router.register(r'owners', views.OwnerViewSet, basename="owner_viewset")
 # router.register(r'interests', views.InterestedViewSet, basename="interests_viewset")
