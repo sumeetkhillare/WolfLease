8bee0b31ece4fb4755cc94e2b6033a149d5a8501
subodhgujar <subodhsgujar@gmail.com>
2022-10-09 09:45:31 -0400

    Commented authentication

--- wolf_lease/settings.py
+++ wolf_lease/settings.py
@@ -72,7 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wolf_lease.wsgi.application'
 
-
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ],
+}
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
