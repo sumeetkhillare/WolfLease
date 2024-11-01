fff0e8edd43a3bb126633dc1952b109e9a3f1917
xpincsu <xpi@ncsu.edu>
2024-11-01 12:55:24 -0400

    xpi - merge fix

--- housing/apps.py
+++ housing/apps.py
@@ -1,19 +1,10 @@
-"""
-    This is the application starting point where you can register different modules. 
-"""
-
 from django.apps import AppConfig
 
 
-
 class HousingConfig(AppConfig):
-    """
-    This is configuration for Housing Module.
-    
-    """
-    default_auto_field = 'django.db.models.BigAutoField'  
+    default_auto_field = 'django.db.models.BigAutoField'
     '''Default autofields '''
     name = 'housing'
 
     def ready(self):
-        import housing.signals 
+        import housing.signals
